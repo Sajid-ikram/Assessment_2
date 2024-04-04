7f9a2332841480f67daf1177cf0906ce7457d64b
Sajid-ikram <sajed.ikram@yahoo.com>
2024-04-04 21:55:36 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:assessment_2/view/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Secondhand Shop For Students',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
